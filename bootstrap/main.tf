a0c21c1791b774c9327ddce2f21896ca08d36e4c
Rahman Khaleel <khaleels@gmail.com>
2025-07-23 18:48:49 -0400

    removing dumm s3 bucket

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -9,14 +9,14 @@ resource "aws_s3_bucket" "tf_state" {
 
 
 #creating test bucket
-resource "aws_s3_bucket" "tf_statetest_bucket" {
-  bucket = "dummy-terraform-state-bucket-test-7104"
+# resource "aws_s3_bucket" "tf_statetest_bucket" {
+#   bucket = "dummy-terraform-state-bucket-test-7104"
 
-  tags = {
-    Name        = "Dummy Terraform State Bucket"
-    Environment = "prod"
-  }
-}
+#   tags = {
+#     Name        = "Dummy Terraform State Bucket"
+#     Environment = "prod"
+#   }
+# }
 # resource "aws_dynamodb_table" "tf_lock" {
 #   name         = var.dynamodb_table
 #   billing_mode = "PAY_PER_REQUEST"
