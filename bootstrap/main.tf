1051b4637f2230944365e75c4a4c250317d6ee00
Rahman Khaleel <khaleels@gmail.com>
2025-07-23 18:07:54 -0400

    removed dynamodb table

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -6,18 +6,18 @@ resource "aws_s3_bucket" "tf_state" {
     Environment = "prod"
   }
 }
-resource "aws_dynamodb_table" "tf_lock" {
-  name         = var.dynamodb_table
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
+# resource "aws_dynamodb_table" "tf_lock" {
+#   name         = var.dynamodb_table
+#   billing_mode = "PAY_PER_REQUEST"
+#   hash_key     = "LockID"
 
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
+#   attribute {
+#     name = "LockID"
+#     type = "S"
+#   }
 
-  tags = {
-    Name        = "Terraform Locks Table"
-    Environment = "prod"
-  }
-}
+#   tags = {
+#     Name        = "Terraform Locks Table"
+#     Environment = "prod"
+#   }
+# }
