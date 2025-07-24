858af896a2bba3140644009b70f268d46a98a079
Rahman Khaleel <khaleels@gmail.com>
2025-07-23 20:28:33 -0400

    adding Dev env

--- bootstrap/backend.tf
+++ bootstrap/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "terraform-state-bucket-prod-7104"
-    key            = "dev/network/terraform.tfstate"   # uniquely identifies state per environment/module
+    key            = "prod/network/terraform.tfstate"   # uniquely identifies state per environment/module
     region         = "us-east-1"
     dynamodb_table = "terraform-locks-prod-7104"
     encrypt        = true
